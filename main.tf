d26b913cbd6d364b4428144ff37422cd3367abe9
Ankit Garg <ankit@example.com>
2021-10-07 20:51:07 +0530

    from scratch

--- main.tf
+++ main.tf
@@ -11,25 +11,11 @@ terraform {
 provider "aws" {
 }
 
-# resource "aws_vpc" "terra" {
-#   cidr_block = "10.0.0.0/16"
-# }
- 
-module "terra-test1" {
-  source = "./modules/network/vpc"
-
-  cidr          = "11.0.0.0/16"
+resource "aws_vpc" "myvpc" {
+  cidr_block  =  "15.0.0.0/16"
 }
 
-module "private-1a" {
-  source = "./modules/network/subnets"
+output "vpc" {
 
-  vpc_id     = aws_vpc.main.id
-  cidr       = "11.0.1.0/24"
-  azone      = "us-west-1a"
-}  
-
-output "vpc_id" {
-
-    value = aws_vpc.main.id
+    value = aws_vpc.myvpc.id
 }
