4a8e2c0e3dd78fc01769d15e8f0de0a848b2675e
Ankit Garg <ankit@example.com>
2021-10-07 16:24:01 +0530

    added vpc

--- main.tf
+++ main.tf
@@ -15,4 +15,9 @@ resource "aws_vpc" "terra-test" {
   cidr_block = "10.0.0.0/16"
 }
 
+module "terra-test1" {
+  source="./modules/"
 
+  cidr_block           = "11.0.0.0/16"
+
+}
---
+++ modules/variables.tf
@@ -0,0 +1,23 @@
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC."
+  type        = string
+  default     = "default"
+}
+
+variable "enable_dns_hostnames" {
+  description = "A boolean flag to enable/disable DNS hostnames in the VPC."
+  type        = bool
+  default     = "true"
+}
+
+variable "enable_dns_support" {
+  description = "A boolean flag to enable/disable DNS support in the VPC."
+  type        = bool
+  default     = "true"
+}
