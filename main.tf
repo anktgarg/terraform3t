81a874040261f145ae163993109d88571a16b863
Ankit Garg <ankit@example.com>
2021-10-07 17:30:20 +0530

    subnet

--- main.tf
+++ main.tf
@@ -11,21 +11,25 @@ terraform {
 provider "aws" {
 }
 
+# resource "aws_vpc" "terra" {
+#   cidr_block = "10.0.0.0/16"
+# }
+ 
 module "terra-test1" {
   source = "./modules/network/vpc"
 
   cidr          = "11.0.0.0/16"
 }
 
-# module "private-1a" {
-#   source = "./modules/network/subnets"
+module "private-1a" {
+  source = "./modules/network/subnets"
 
-#   vpc_id     = aws_vpc.terra-test1.id
-#   cidr       = "11.0.1.0/24"
-#   azone      = "us-west-1a"
-# }  
+  vpc_id     = aws_vpc.main.id
+  cidr       = "11.0.1.0/24"
+  azone      = "us-west-1a"
+}  
 
 output "vpc_id" {
 
-    value = "aws_vpc.terra-test1.id"
+    value = aws_vpc.main.id
 }
