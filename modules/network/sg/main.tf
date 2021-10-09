c8675140c06ecf4ca7abb236e3a69532fc084325
Ankit Garg <ankit@example.com>
2021-10-09 11:42:33 +0530

    sg

--- modules/network/sg/main.tf
+++ modules/network/sg/main.tf
@@ -2,15 +2,15 @@ resource "aws_security_group" "sg" {
 
   vpc_id      = var.vpc_id
 
-  ingress = [
-    {
-      description      = "Web from VPC"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = "0.0.0.0/0"
-    }
-  ]
+  # ingress = [
+  #   {
+  #     description      = "Web from VPC"
+  #     from_port        = 80
+  #     to_port          = 80
+  #     protocol         = "tcp"
+  #     cidr_blocks      = "0.0.0.0/0"
+  #   }
+  # ]
 
   # egress = [
   #   {
