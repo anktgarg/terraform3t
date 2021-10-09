fe221a7de5247b11d417104b9bcc8ec8043369e4
Ankit Garg <ankit@example.com>
2021-10-09 14:58:26 +0530

    rta

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 # Configure the AWS Provider
 provider "aws" {
   region    = "us-west-2"
@@ -13,45 +12,45 @@ module "main_vpc" {
     ), var.default_tags)
 }
 
-# module "private-2a" {
-#   source    = "./modules/network/subnets"
-#   vpc_id    = module.main_vpc.vpc_id
-#   cidr      = var.cidr_private-2a
-#   availability_zone     = var.availability_zone-2a
-#   default_tags = merge(tomap(
-#       {"Name" = "Test-private-2a"}
-#     ), var.default_tags)
-# }  
+module "private-2a" {
+  source    = "./modules/network/subnets"
+  vpc_id    = module.main_vpc.vpc_id
+  cidr      = var.cidr_private-2a
+  availability_zone     = var.availability_zone-2a
+  default_tags = merge(tomap(
+      {"Name" = "Test-private-2a"}
+    ), var.default_tags)
+}  
 
-# module "private-2b" {
-#   source    = "./modules/network/subnets"
-#   vpc_id    = module.main_vpc.vpc_id
-#   cidr      = var.cidr_private-2b
-#   availability_zone     = var.availability_zone-2b
-#   default_tags = merge(tomap(
-#       {"Name" = "Test-private-2b"}
-#     ), var.default_tags)
-# }
+module "private-2b" {
+  source    = "./modules/network/subnets"
+  vpc_id    = module.main_vpc.vpc_id
+  cidr      = var.cidr_private-2b
+  availability_zone     = var.availability_zone-2b
+  default_tags = merge(tomap(
+      {"Name" = "Test-private-2b"}
+    ), var.default_tags)
+}
 
-# module "public-2a" {
-#   source    = "./modules/network/subnets"
-#   vpc_id    = module.main_vpc.vpc_id
-#   cidr      = var.cidr_public-2a
-#   availability_zone     = var.availability_zone-2a
-#   default_tags = merge(tomap(
-#       {"Name" = "Test-public-2a"}
-#     ), var.default_tags)
-# }
+module "public-2a" {
+  source    = "./modules/network/subnets"
+  vpc_id    = module.main_vpc.vpc_id
+  cidr      = var.cidr_public-2a
+  availability_zone     = var.availability_zone-2a
+  default_tags = merge(tomap(
+      {"Name" = "Test-public-2a"}
+    ), var.default_tags)
+}
 
-# module "public-2b" {
-#   source    = "./modules/network/subnets"
-#   vpc_id    = module.main_vpc.vpc_id
-#   cidr      = var.cidr_public-2b
-#   availability_zone     = var.availability_zone-2b
-#   default_tags = merge(tomap(
-#       {"Name" = "Test-public-2b"}
-#     ), var.default_tags)
-# }
+module "public-2b" {
+  source    = "./modules/network/subnets"
+  vpc_id    = module.main_vpc.vpc_id
+  cidr      = var.cidr_public-2b
+  availability_zone     = var.availability_zone-2b
+  default_tags = merge(tomap(
+      {"Name" = "Test-public-2b"}
+    ), var.default_tags)
+}
 
 module "igw" {
   source    = "./modules/network/igw"
@@ -64,15 +63,18 @@ module "igw" {
 module "public-rt" {
   source    = "./modules/network/routetable"
   vpc_id    = module.main_vpc.vpc_id
-    default_tags = merge(tomap(
+  default_tags = merge(tomap(
       {"Name" = "Test-public-rt"}
     ), var.default_tags)
 }
 
-# module "private-rt" {
-#   source    = "./modules/network/routetable"
-#   vpc_id    = module.main_vpc.vpc_id
-# }
+module "private-rt" {
+  source    = "./modules/network/routetable"
+  vpc_id    = module.main_vpc.vpc_id
+  default_tags = merge(tomap(
+      {"Name" = "Test-private-rt"}
+    ), var.default_tags)
+}
 
 module "public-routes" {
   source    = "./modules/network/routes"
@@ -81,17 +83,30 @@ module "public-routes" {
   gateway_id                = module.igw.igw_id
 }
 
-# module "rta-2a" {
-#   source    = "./modules/network/rt-association"
-#   subnet_id    = module.public-2a.subnet_id
-#   route_table_id    = module.public-rt.route_table_id
-# }
+module "rta-public-2a" {
+  source    = "./modules/network/rt-association"
+  subnet_id    = module.public-2a.subnet_id
+  route_table_id    = module.public-rt.route_table_id
+}
 
-# module "rta-2b" {
-#   source    = "./modules/network/rt-association"
-#   subnet_id    = module.public-2b.subnet_id
-#   route_table_id   = module.public-rt.route_table_id
-# }
+module "rta-public-2b" {
+  source    = "./modules/network/rt-association"
+  subnet_id    = module.public-2b.subnet_id
+  route_table_id   = module.public-rt.route_table_id
+}
+
+module "rta-private-2a" {
+  source    = "./modules/network/rt-association"
+  subnet_id    = module.private-2a.subnet_id
+  route_table_id    = module.private-rt.route_table_id
+}
+
+module "rta-private-2b" {
+  source    = "./modules/network/rt-association"
+  subnet_id    = module.private-2b.subnet_id
+  route_table_id   = module.private-rt.route_table_id
+}
+
 
 # data "aws_ami" "ubuntu" {
 #   most_recent = true
