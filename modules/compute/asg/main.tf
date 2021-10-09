c825fb4ac9d563264d1541fe7a957afc8805d53e
Ankit Garg <ankit@example.com>
2021-10-09 12:48:39 +0530

    asg

--- modules/compute/asg/main.tf
+++ modules/compute/asg/main.tf
@@ -1,7 +1,7 @@
 resource "aws_autoscaling_group" "asg" {
 
   launch_configuration = var.launch_configuration
-  subnet_ids  = ["${var.subnet_ids}"]
+  subnet_ids  = var.subnet_ids
 
   min_size                  = var.min_size
   max_size                  = var.max_size
