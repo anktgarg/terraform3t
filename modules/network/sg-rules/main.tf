248078ab620fbee7725e52eb8eff1e884b93dc9d
Ankit Garg <ankit@example.com>
2021-10-09 16:06:49 +0530

    sg

--- modules/network/sg-rules/main.tf
+++ modules/network/sg-rules/main.tf
@@ -1,9 +1,9 @@
-resource "aws_security_group_rule" "example" {
+resource "aws_security_group_rule" "sg-rules" {
   type              = var.type
   from_port         = var.from_port
   to_port           = var.to_port
   protocol          = var.protocol
-  cidr_blocks       = ["${var.cidr_blocks}"]
+  cidr_blocks       = var.cidr_blocks
   security_group_id = var.security_group_id
   description       = var.description
 }
