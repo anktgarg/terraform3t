f4f344f9ed97d4da3e1483c2cb59e5285962acf7
Ankit Garg <ankit@example.com>
2021-10-09 11:47:41 +0530

    sg

--- modules/compute/launchconfig/main.tf
+++ modules/compute/launchconfig/main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_configuration" "this" {
   image_id                    = var.image_id
   instance_type               = var.instance_type
   key_name                    = var.key_name
-  security_groups             = var.security_groups
+  security_groups             = ["${var.security_groups}"]
   associate_public_ip_address = var.associate_public_ip_address
 
   lifecycle {
